71c477b5752dd46fb7a7713ff8354661c325e8cf
mhdaouda <daoudayinde@gmail.com>
2025-06-06 18:36:48 +0100

    fix: restauration de la configuration locale sur dev

--- next.config.js
+++ next.config.js
@@ -4,13 +4,9 @@ const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // Nécessaire pour la génération statique
   images: {
     unoptimized: true,
-  },
-  basePath: '/gloriam-consulting', // Le nom de votre dépôt
-  assetPrefix: '/gloriam-consulting/', // Le nom de votre dépôt avec un slash à la fin
-  trailingSlash: true, // Ajoute un slash à la fin des URLs
+  }
 };
 
 module.exports = withNextIntl(nextConfig); 
